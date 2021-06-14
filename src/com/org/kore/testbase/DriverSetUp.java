f421f0beca979405621c2bc5bec4f7b631d2cb2a
JayakrishnaD <jayakrishna.dandru@kore.com>
2021-06-14 19:21:06 +0530

    Repo 10

--- src/com/org/kore/testbase/DriverSetUp.java
+++ src/com/org/kore/testbase/DriverSetUp.java
@@ -346,11 +346,11 @@ public class DriverSetUp {
 					"<html><head></head>");
 			writer.write("<body><h2> WorkAssist: "+App+" Env</b> WebApp Automation Results</h2></body>");
 			
-			/*writer.write("<body><table><table border ='1'><tr><th><p style=text-align:left><b>Scope</b></p></th><td> <a href="+"https://docs.google.com/spreadsheets/d/1Q3aIa9lp_im-4k6athtNH8RVgJ9KfWMfnyhppFIds6A/edit?ts=5fd77590#gid=1327035589"+"> Basic Sanity test cases</a></td><tr><th><b>Environment</b>&emsp;</th><td>"+workingurl+"  <b>("+App+"  )</b>"+"</td></tr><tr><th><p style=text-align:left><b>Browser</b></p></th><td>Chrome</td></tr></body>");
+			/*writer.write("<body><table><table border ='1'><tr><th><b>Scope</b></th><td> <a href=https://docs.google.com/spreadsheets/d/1Q3aIa9lp_im-4k6athtNH8RVgJ9KfWM=fnyhppFIds6A/edit?ts=3D5fd77590#gid=3D1327035589> Basic sanity test cases</a></td><tr><th><b>Environment</b>&emsp;</th><td><b>https://workassist-qa.kore.ai/ (QA)</b></td> </tr><tr><th><b>Browser</b></th><td>Chrome</td></tr></body>");
 			writer.write("<body><h2> </h2></body>");
 			writer.write("<table> </table>");*/
 			
-			writer.write("<body><table><table border ='1'><tr><th><p style=text-align:left;>Scope</th><td> <a href=https://docs.google.com/spreadsheets/d/1Q3aIa9lp_im-4k6athtNH8RVgJ9KfWM=fnyhppFIds6A/edit?ts=3D5fd77590#gid=3D1327035589> Basic sanity test cases</a></td><tr><th><b>Environment</b>&emsp;</th><td><b>https://workassist-qa.kore.ai/ (QA)</b></td> </tr><tr><th><b>Browser</b></th><td>Chrome</td></tr></body>");
+			writer.write("<body><table><tr><th><b>Scope</b></th><td> <a href=https://docs.google.com/spreadsheets/d/1Q3aIa9lp_im-4k6athtNH8RVgJ9KfWM=fnyhppFIds6A/edit?ts=3D5fd77590#gid=3D1327035589> Basic sanity test cases</a></td><tr><th><b>Environment</b>&emsp;</th><td><b>https://workassist-qa.kore.ai/ (QA)</b></td> </tr><tr><th><b>Browser</b></th><td>Chrome</td></tr></body>");
 			writer.write("<body><h2> </h2></body>");
 			writer.write("<table> </table>");
 			
