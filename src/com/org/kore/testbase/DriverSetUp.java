460479a910ade8cb2f19683dfce53f3f8a3a6f09
JayakrishnaD <jayakrishna.dandru@kore.com>
2021-06-14 18:53:37 +0530

    repo 7

--- src/com/org/kore/testbase/DriverSetUp.java
+++ src/com/org/kore/testbase/DriverSetUp.java
@@ -350,7 +350,7 @@ public class DriverSetUp {
 			writer.write("<body><h2> </h2></body>");
 			writer.write("<table> </table>");*/
 			
-			writer.write("<body><table> <table border =3D'1'><tr><th><p style=text-align:left><b>Scope</b></p></th><td> <a href=https://docs.google.com/spreadsheets/d/1Q3aIa9lp_im-4k6athtNH8RVgJ9KfWM=fnyhppFIds6A/edit?ts=3D5fd77590#gid=3D1327035589> Basic sanity test cases</a></td><tr><th><b>Environment</b></th><td><b>https://workassist-qa.kore.ai/ (QA)</b></td> </tr><tr><th><b>Browser</b></th><td>Chrome</td></tr></body>");
+			writer.write("<body><table> <table border =3D'1'><tr><th><b>Scope</b></th><td> <a href=https://docs.google.com/spreadsheets/d/1Q3aIa9lp_im-4k6athtNH8RVgJ9KfWM=fnyhppFIds6A/edit?ts=3D5fd77590#gid=3D1327035589> Basic sanity test cases</a></td><tr><th><b>Environment</b></th><td><b>https://workassist-qa.kore.ai/ (QA)</b></td> </tr><tr><th><b>Browser</b></th><td>Chrome</td></tr></body>");
 			
 			
 			writer.write("<body><table><table border ='1'><tr bgcolor="+"#02c8ff"+"><th><font color="+"white"+"><b>Scenarios Status</b></th><th><font color="+"white"+"><b>Scenarios Count</b></th></tr><tr><td><font color="+"green"+"><b>PASS</b></font></td><td style="+"text-align:center"+">" + passcount
